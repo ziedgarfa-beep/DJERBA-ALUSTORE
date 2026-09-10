23205c545f5379a03d96f959659f49f1b64f3d4f
ziedgarfa-beep <ziedgarfa@gmail.com>
2026-09-10 16:59:59 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='djerba-alustore-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+});
